18ccfa81495cd549b339e315bcc25d076f45e87c
Sueyasu <yasukexxx@gmail.com>
2019-10-12 00:44:28 +0900

    Add retry db connection

--- registries/postgres.go
+++ registries/postgres.go
@@ -6,6 +6,7 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"log"
 	"os"
+	"time"
 )
 
 type Postgres struct {
@@ -13,9 +14,27 @@ type Postgres struct {
 }
 
 func NewPostgres() Postgres {
-	db, err := gorm.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PASSWORD")))
+	db, err := RetryConnection(func() (db *gorm.DB, err error) {
+		db, err = gorm.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PASSWORD")))
+		return
+	}, 10)
 	if err != nil {
 		log.Fatal(err)
 	}
 	return Postgres{db}
+}
+
+type connect func() (*gorm.DB, error)
+
+func RetryConnection(fnc connect, retry int) (db *gorm.DB, err error) {
+	for i := 0; i < retry; i++ {
+		db, err = fnc()
+		if err != nil {
+			log.Print(err)
+			time.Sleep(10 * time.Second)
+			continue
+		}
+		return
+	}
+	return db, fmt.Errorf("Failed connect to db")
 }
